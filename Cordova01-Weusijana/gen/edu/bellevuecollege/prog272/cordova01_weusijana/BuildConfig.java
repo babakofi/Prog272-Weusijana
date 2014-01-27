88cfea21ccae29051a0d4fa3173ee873e8931630
Baba Kofi Weusijana <kofi@probook.(none)>
2014-01-27 01:54:04 -0800

    init Cordova01-Weusijana

---
+++ Cordova01-Weusijana/gen/edu/bellevuecollege/prog272/cordova01_weusijana/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.bellevuecollege.prog272.cordova01_weusijana;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
