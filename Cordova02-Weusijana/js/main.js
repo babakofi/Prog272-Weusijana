0dd1527aaa12cc2f95173d2fe49b774627422e97
Baba Kofi Weusijana <kofi@probook.(none)>
2014-01-27 23:59:34 -0800

    fixed

--- Cordova02-Weusijana/js/main.js
+++ Cordova02-Weusijana/js/main.js
@@ -28,3 +28,24 @@ var Converter = {
         }
     }
 };
+var $f_input = $('#f_input'), $fc_button = $('#fc_button'), $c_output = $('#c_output');
+var fc_buttonClickHandler = function(event) {"use strict";
+    event.preventDefault();
+    $c_output.text('');
+    $c_output.text(Converter.fahrenheitToCelsius($f_input.val()));
+};
+var $m_input = $('#m_input'), $mk_button = $('#mk_button'), $k_output = $('#k_output');
+var mk_buttonClickHandler = function(event) {"use strict";
+    event.preventDefault();
+    $k_output.text('');
+    $k_output.text(Converter.milesToKilometers($m_input.val()));
+};
+var $x_input=$('#x_input'), $sqrt_button = $('#sqrt_button'), $sqrtx_output = $('#sqrtx_output');
+var sqrt_buttonClickHandler = function(event) {"use strict";
+    event.preventDefault();
+    $sqrtx_output.text('');
+    $sqrtx_output.text(Converter.sqrt($x_input.val()));
+};
+$fc_button.click(fc_buttonClickHandler);
+$mk_button.click(mk_buttonClickHandler);
+$sqrt_button.click(sqrt_buttonClickHandler);
