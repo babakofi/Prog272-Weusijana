b702377eb7e302228cf55a6d4821df9cffef4fe8
Baba Kofi Weusijana <kofi@probook.(none)>
2014-02-04 12:40:42 -0800

    Implemented Part1 and started a test for InfoMan.loadParagraph

--- Week04AssignmentWeusijana/assets/www/js/InfoMan.js
+++ Week04AssignmentWeusijana/assets/www/js/InfoMan.js
@@ -2,21 +2,28 @@
  * @author Baba Kofi Weusijana
  */
 
-function InfoMan() {   
-    
+function InfoMan() {
+
     var that = this;
     var feetInOneMile = 5280;
-    
+
     var feetToMilesPrivate = function() {
         var miles = $("#dataInput").val();
         var result = that.feetToMiles(miles);
         $("#dataInput").val(result);
     };
-    
-    this.feetToMiles = function(miles) {
-        return miles * feetInOneMile;
+
+    this.loadParagraph = function(event) {
+        var idString = $(this).attr("id");
+        if (idString) {
+            var numberString = idString.slice(-2);
+            if (numberString) {
+                $("#div" + numberString).load("Sources.html #paragraph" + numberString);
+            }
+        }
     };
-    
-    $("#milesButton").click(feetToMilesPrivate);
-    
+
+    $("button").click(this.loadParagraph);
+
+    console.log("InfoMan created and inititalized");
 }
--- Week04AssignmentWeusijana/assets/www/test/TestInfoMan.js
+++ Week04AssignmentWeusijana/assets/www/test/TestInfoMan.js
@@ -4,14 +4,9 @@
 
 describe("Test InfoMan", function() {
     
-    it("tests milesToFeet(1) to yield 5280", function() {
-       var convert = new InfoMan();
-       expect(convert.feetToMiles(1)).toBe(5280); 
+    it("expects loadParagraph(mockEvent) to load paragraph01", function() {
+       var infoMan = new InfoMan();
+       // expect(infoMan.loadParagraph(mockEvent)).toBe(5280); 
     });
-    
-    it("tests milesToFeet(2) to yield 10560", function() {
-       var convert = new InfoMan();
-       expect(convert.feetToMiles(2)).toBe(10560); 
-    });
-    
+     
 });
