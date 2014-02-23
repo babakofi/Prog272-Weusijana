4f2397ea6e70b8bad32e0a0142fb9a1d1e89cbcb
Baba Kofi Weusijana <kofi@probook.(none)>
2014-02-23 05:30:23 -0800

    Completed QueryMongo for setting up the test_data collection in the test database

--- Week05-NodeExpressMongo/QueryMongo.js
+++ Week05-NodeExpressMongo/QueryMongo.js
@@ -1,11 +1,15 @@
 /**
  * @author Charlie Calvert
+ * @author Baba Kofi Weusijana
  */
 
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var format = require('util').format;
+var database = null;
+var numberOfRecordsToInsert = 250;
+var collectionName = 'test_data';
 
 var QueryMongo = (function() {
 
@@ -21,20 +25,26 @@ var QueryMongo = (function() {
     QueryMongo.prototype.getData = function(result) {
         console.log('Called getData');
         // Open the test database that comes with MongoDb
-        MongoClient.connect(url01, function(err, database) {
+        MongoClient.connect(url01, function(err, db) {
+            console.log('In getDataCallback');
             if (err) {
                 throw err;
             }
-            console.log('IngetDataCallback');
-            // insertIntoCollection(database, 'test_insert', { f : 7 });
-            getCollection(database, result);
+            database = db;
+            console.log('database:', database);
+            // insertIntoCollection(database, collectionName, { f : 7 });
+            // getCollection(database, result);
         });
 
     };
-    
-    var getCollection = function(database, response) {
 
-        var collection = database.collection('test_insert');
+    QueryMongo.prototype.connect = function(afterConnectCallbackFunction) {
+        console.log('Called connect');
+        // Open the test database that comes with MongoDb
+        MongoClient.connect(url01, afterConnectCallbackFunction);
+    };
+
+    var getCollection = function(database, collection, response) {
 
         // Count documents in the collection
         collection.count(function(err, count) {
@@ -46,7 +56,7 @@ var QueryMongo = (function() {
             console.dir(results);
             // $("#mongoData").html(results);
             database.close();
-            var body = '<html><body><h2>Mongo Data: ' + results[2].firstName + '</h2></body></html>';
+            var body = '<html><body><h2>Mongo Data: ' + results[0].firstName + '</h2></body></html>';
             response.setHeader('Content-Type', 'text/html');
             response.setHeader('Content-Length', Buffer.byteLength(body));
             response.end(body);
@@ -55,11 +65,29 @@ var QueryMongo = (function() {
     };
 
     // Will create collection if it does not exist
-    var insertIntoCollection = function(db, collectionName, objectToInsert) {
+    QueryMongo.prototype.insertIntoCollection = function(database, collectionName, objectToInsert) {
 
-        var collection = db.collection(collectionName);
+        var collection = database.collection(collectionName);
         collection.insert(objectToInsert, function(err, docs) {
-            getCollection(db);
+            if (err) {
+                throw err;
+            }
+            // database.close();
+            console.log("insert succeeded");
+            collection.count(function(err, count) {
+                if (err) {
+                    throw err;
+                } else {
+                    console.log("collection.count:", count);
+                    if (count >= numberOfRecordsToInsert) {
+                        try {
+                            database.close();
+                        } catch(closeerr) {
+                            console.dir(closeerr);
+                        }
+                    }
+                }
+            });
         });
     };
 
@@ -67,18 +95,51 @@ var QueryMongo = (function() {
 
 })();
 
-app.get('/', function(request, response) {
-    console.log('request:', request);
-    console.log('response:', response);
-    var q = new QueryMongo();
-    var data = q.getData(response);
-    console.log('data:', data);
-    console.log('response:', response);
+// var closeWhenAllRecordsInserted = function(collection) {
+// // Perform a total count command
+// collection.count(function(err, count) {
+// if (err) {
+// throw err;
+// } else {
+// console.log("count:", count);
+// if (count >= numberOfRecordsToInsert) {
+// database.close();
+// } else {
+// closeWhenAllRecordsInserted(collection);
+// }
+// }
+// });
+// };
+
+var queryMongo = new QueryMongo();
+queryMongo.connect(function(err, db) {
+    console.log("In queryMongo.connect's callback");
+    if (err) {
+        throw err;
+    }
+    database = db;
+    // console.log('database:', database);
+
+    // Erase the old collection if it exists
+    var collection = database.collection(collectionName);
+    collection.remove(function(err) {
+        if (err) {
+            throw err;
+        }
+        // Run a loop to generate the new data
+        for (var recNum = 0; recNum < numberOfRecordsToInsert; recNum++) {
+            var data = {
+                "firstName" : "Rita10" + recNum,
+                "lastName" : "Hill10" + recNum,
+                "address" : "10" + recNum + " Ruby Street",
+                "city" : "Bellevue",
+                "state" : "WA",
+                "zip" : "98002"
+            };
+            // console.log('data:', data);
+            if (data) {
+                queryMongo.insertIntoCollection(database, collectionName, data);
+            }
+        }
+    });
 });
-
-app.listen(30025);
-console.log('Listening on port 30025');
-
-/* $(document).ready(function() {
- var q = new QueryMongo();
- }); */
--- Week05-NodeExpressMongo/README.md
+++ Week05-NodeExpressMongo/README.md
@@ -1,5 +1,6 @@
-NodeExpressMongo01
-==================
+Week05-NodeExpressMongo
+=======================
+*by Baba Kofi Weusijana*
 
 This applications contains:
 
@@ -8,5 +9,16 @@ This applications contains:
 - MongoDb
 
 It assumes that you have a MongoDB installed named ***test***.
-- Run "node QueryMongo.js" to insert some data into the MongoDb database named ***test***.
-- Run "node server.js" to start the Express webserver and allow Web clients to use the app
+
+1. Run "node QueryMongo.js" to insert some data into the MongoDb database named ***test***.
+2. Run "node server.js" to start the Express webserver and allow Web clients to use the WebApp. It will display any one record from the array of records to the user.  
+The WebApp will retrieve the array from the server only once. 
+This should occur immediately after your application loads.
+Once the array is on the client side, the user will be able to select any one item from the array.
+
+Other features of the WebApp are:
+
+- A place to show the selected record retrieved from the server. 
+This would probably be a series of paragraph tags, but other options are acceptable.
+- An input element where the user can enter the number of the item they want to retreive.
+- A button to press to activate the display of the next item.
