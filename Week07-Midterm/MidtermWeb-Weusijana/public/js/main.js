86d1ab059622bae28787aee259ee64d1b46320d3
Baba Kofi Weusijana <kofi@probook.(none)>
2014-03-01 15:11:14 -0800

    Buttons for each sonnet

--- Week07-Midterm/MidtermWeb-Weusijana/public/js/main.js
+++ Week07-Midterm/MidtermWeb-Weusijana/public/js/main.js
@@ -8,6 +8,7 @@ var App = (function() {'use strict';
     function App() {
         $("#readAll").click(readAll);
         $("#showData").click(showData);
+        $("#search").click(searchForKeywords);
 
         readAll();
     }
@@ -17,12 +18,34 @@ var App = (function() {'use strict';
         $('#title').html(sonnets[index].title);
         $('#content').html(sonnets[index].content);
         $('#author').html(sonnets[index].author);
-        $('#keywords').html(sonnets[index].keywords);
+        var keywords = sonnets[index].keywords;
+        if (keywords) {
+            $('#keywords').html("<strong>Keywords:</strong>");
+            for (var i = 0; i < keywords.length; i++) {
+                $('#keywords').append("<span>" + keywords[i] + "</span>&nbsp;");
+            }
+        }
     };
 
     var showData = function() {
         var index = $("#userIndex").val();
-        displayRecord(index);
+        if (index) {
+            if ((!isNaN(index)) && (index > -1)) {
+                index--;
+                displayRecord(index);
+                return;
+            }
+        }
+        // Something must have gone wrong, reset the userIndex
+        $("#userIndex").val(1);
+    };
+
+    var showTitle = function(event) {
+        console.log("event:", event);
+        console.log("this:", this);
+        console.log("this.value:", this.value);
+        $("#userIndex").val(parseInt(this.value) + 1);
+        showData();
     };
 
     var readAll = function() {
@@ -30,14 +53,49 @@ var App = (function() {'use strict';
         $.getJSON('/poems', function(data) {
             console.log(data[0]);
             sonnets = data[0].sonnets;
-            displayRecord(0);
-            $("#mongoData").empty();
-            for (var i = 0; i < sonnets.length; i++) {
-                $("#mongoData").append('<li>' + JSON.stringify(sonnets[i]) + '</li>');
+            if (sonnets) {
+                resetUI();
+                displayRecord(0);
+                $("#mongoData").empty();
+                for (var i = 0; i < sonnets.length; i++) {
+                    var title = sonnets[i].title;
+                    if (title) {
+                        $("#titles").append('<button class="title" value="' + i + '" type="button">' + title + '</button>');
+                    }
+                    $("#mongoData").append('<li>' + JSON.stringify(sonnets[i]) + '</li>');
+                }
+                $(".title").click(showTitle);
             }
         });
     };
 
+    var searchForKeywords = function(event) {
+        console.log("searchForKeywords called");
+        $.getJSON('/poems', function(data) {
+            console.log(data[0]);
+            sonnets = data[0].sonnets;
+            if (sonnets) {
+                resetUI();
+                displayRecord(0);
+                $("#mongoData").empty();
+                for (var i = 0; i < sonnets.length; i++) {
+                    var title = sonnets[i].title;
+                    if (title) {
+                        $("#titles").append('<button class="title" value="' + i + '" type="button">' + title + '</button>');
+                    }
+                    $("#mongoData").append('<li>' + JSON.stringify(sonnets[i]) + '</li>');
+                }
+                $(".title").click(showTitle);
+            }
+        });
+    };
+
+    var resetUI = function() {
+        $("#keywordInputs").val("Enter Keywords");
+        $("#titles").empty();
+        $("#userIndex").val(1);
+    };
+    
     return App;
 })();
 
